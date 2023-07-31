0de60c9b7452c27febece612ab8631a562726eed
ngomanhthong0812 <139620427+ngomanhthong0812@users.noreply.github.com>
2023-07-31 07:48:07 +0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,6 @@
+var anMenu = document.querySelector(".icon-menu-right")
+var menu = document.querySelector(".menu-right")
+
+anMenu.addEventListener('click', function(){
+    menu.classList.toggle("menu-right-none")
+})
